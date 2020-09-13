14b304d0ba1027b6741084e36227eeaa9d4582bc
JustelisJ <j.jancys@gmail.com>
2020-09-13 13:43:23 +0300

    Created the post new article template and the form. Missing proccessing the form

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,10 +2,15 @@
 namespace App\Controller;
 
 use App\Entity\Article;
-
+use Doctrine\DBAL\Types\TextType as TypesTextType;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ArticleController extends AbstractController
 {
@@ -18,6 +23,32 @@ class ArticleController extends AbstractController
         $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
 
         return $this->render('articles/index.html.twig', array("articles" => $articles));
+    }
+
+    /**
+     * @Route("/article/new", name="new_article", methods={"GET", "POST"})
+     */
+    public function new(Request $request)
+    {
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+            ->add("title", TextType::class, array('attr' => 
+                array('class' => 'form-control')
+            ))
+            ->add("body", TextareaType::class, array(
+                'required'=> false,
+                'attr' => array('class' => 'form-control')
+            ))
+            ->add("save", SubmitType::class, array(
+                'label' => 'Create article',
+                'attr' => array('class' => 'btn btn-primary mt-3')
+            ))
+            ->getForm();
+
+        return $this->render("articles/new.html.twig", array(
+            'form' => $form->createView()
+        ));
     }
 
     /**
